2a1b359dac2020a09488232b72865f85b39cfffb
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-07-14 17:11:17 +0530

    updated

--- app/src/main/java/com/example/android_ndk_sample/MainActivity.kt
+++ app/src/main/java/com/example/android_ndk_sample/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    private external fun stringFromJNI(): String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
